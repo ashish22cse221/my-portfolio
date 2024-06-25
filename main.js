7a2debe714fe4559689339eb4e5f09abd81a6d19
ASHISH CHOUDHURY <151498222+ashish22cse221@users.noreply.github.com>
2024-06-25 20:52:48 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,37 @@
-var typed=new Typed(".text",{
-    strings:["Front-end Developer","Web Developer","C-coder","Python-coder"],
-    typeSpeed:100,
-    backSpeed:90,
-    backDelay:1000,
-    loop:true
-})
+var typed = new Typed(".text", {
+    strings: ["Front-end Developer", "Web Developer", "C-coder", "Python-coder"],
+    typeSpeed: 100,
+    backSpeed: 90,
+    backDelay: 1000,
+    loop: true
+});
+
+document.getElementById('email-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    var form = this;
+    var formData = new FormData(form);
+
+    fetch("https://formspree.io/f/xqazzgoz", {
+        method: "POST",
+        body: formData,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            alert("Thanks for your submission!");
+            form.reset();
+        } else {
+            response.json().then(data => {
+                if (Object.hasOwn(data, 'errors')) {
+                    alert(data["errors"].map(error => error["message"]).join(", "));
+                } else {
+                    alert("Oops! There was a problem submitting your form");
+                }
+            });
+        }
+    }).catch(error => {
+        alert("Oops! There was a problem submitting your form");
+    });
+});
